a1525e5ed9d791b3ab5c7ef24c7bbee25fc806dd
elvis24 <villaca6@hotmail.com>
2023-07-19 23:56:28 -0500

    formulario

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
 from django.views.generic import View
+from .forms import PostCrearForm
+
 
 # Create your views here.
 
 class BlogListView(View):
-    def get(self, request, *args, **kwargs):
+    def get(self, request, *args, **kwargs):       
+        context={           
+        }
+        return render(request, 'blog_list.html', context)
+
+class BlogCreateView(View):
+    def get(self, request, *args, **Kwargs):
+        form=PostCrearForm()
+        context={
+            'form':form
+        }
+        return render(request, 'blog_create.html', context)
+    
+
+    def post(self, request, *args, **Kwargs):
         context={
 
         }
-        return render(request, 'blog_list.html', context)
+        return render(request, 'blog_create.html', context)
